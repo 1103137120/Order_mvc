b95da209725cda79415462105863f98dab0110f7
ASUS <ASUS@ASUS-PCKS>
2017-03-10 22:20:33 +0800

    利用post執行新增訂單的傳值

--- mvc/Controllers/OrderController.cs
+++ mvc/Controllers/OrderController.cs
@@ -20,5 +20,23 @@ namespace mvc.Controllers
             ViewBag.CusName = order.CusName;
             return View();
         }
+        /// <summary>
+        /// 新增訂單的畫面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult InsertOrder() {
+            return View();
+        }
+        /// <summary>
+        /// 新增訂單存檔的Action
+        /// </summary>
+        /// <param name="order"></param>
+        /// <returns></returns>
+        [HttpPost()]
+        public ActionResult InsertOrder(Models.Order order) {
+            Models.OrderService orderService = new Models.OrderService();
+            orderService.InsertOrder(order);
+            return View("Index");
+        }
     }
 }
--- mvc/Models/OrderService.cs
+++ mvc/Models/OrderService.cs
@@ -13,7 +13,7 @@ namespace mvc.Models
         /// <summary>
         /// 新增訂單
         /// </summary>
-        public void InsertOrder() {
+        public void InsertOrder(Models.Order order) {
 
         }
         /// <summary>
