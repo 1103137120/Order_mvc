2f471850d636185e182d42bc84431ceca3f13752
ASUS <ASUS@ASUS-PCKS>
2017-04-06 15:09:06 +0800

    完成：顯示訂單列表、單一訂單明細OK 缺：新增、修改、刪除、多條件查詢、各頁面下拉式選單

--- mvc/Controllers/OrderController.cs
+++ mvc/Controllers/OrderController.cs
@@ -3,49 +3,101 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mvc.Models;
+
 
 namespace mvc.Controllers
 {
     public class OrderController : Controller
     {
-        /// <summary>
-        /// 訂單管理首頁
-        /// </summary>
-        /// <returns></returns>       
+        private OrderService orderService = new OrderService();
         public ActionResult Index()
         {
-            Models.OrderService orderService = new Models.OrderService();
-            ViewBag.getOrderById = orderService.GetOrderById("11069");
-            ViewBag.Data = orderService.GetOrders();
+            return View(orderService.GetOrders());
+        }
 
+        [HttpGet] 
+        public ActionResult Create()
+        {
             return View();
         }
-        /// <summary>
-        /// 新增訂單的畫面
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult InsertOrder() {
-            return View();
+
+        [HttpPost]
+        public ActionResult Create(OrderService orderService)        {
+            
+            if (ModelState.IsValid) orderService.Save();            
+            return RedirectToAction("Index");
+
         }
-        /// <summary>
-        /// 新增訂單存檔的Action
-        /// </summary>
-        /// <param name="order"></param>
-        /// <returns></returns>
-        [HttpPost()]
-        public ActionResult InsertOrder(Models.Order order) {
-            ViewBag.Desc1 = "我是 ViewBag";
-            ViewData["Desc2"] = "我是ViewData";
-            TempData["Desc3"] = "我是TempData";
+        public ActionResult Detail(int? orderId)
+        {
+            return View(orderService.GetOrderById(orderId));
+
+        }
+
+        [HttpGet]
+        //httq://server/home/edit?name=xxx
+        //提供輸入欄位及Submit鈕
+        public ActionResult Edit(int? orderId)
+        {
+            return View(orderService.GetOrderById(orderId));
+        }
+
+        [HttpPost] //httq://server/home/edit?name=xxx 送出表單時
+        public ActionResult Edit(int? orderId, FormCollection form)
+        {
+            //TODO: 實務上應加入更新權限檢核，在此省略
+            //先讀出資料
+            OrderService origPlayer = new OrderService();
+            origPlayer.GetOrderById(orderId);
+
+            //用前端傳回的資料更新Key以外的欄位
+            if (
+                origPlayer != null &&
+                TryUpdateModel<OrderService>(
+                    origPlayer,
+                    //列出要更新的屬性, Name不得更換，故只有Score
+                    //【補充】demo有篇TryUpdateModel介紹: http://demo.tc/Post/655
+                    new string[] { "Score" }, form)
+               )
+                //資料無誤的話才儲存
+                origPlayer.Save();
+            else //更新失敗時
+            {
+                ModelState.AddModelError("UpdateError", "更新失敗!");
+                return View(origPlayer);
+            }
             return RedirectToAction("Index");
         }
-        [HttpGet()]
-        public JsonResult TestJson() {
-            var result = new Models.Order() {
-                CustId = "ABC",
-                CustName="測試JSON"
-            };
-            return this.Json(result, JsonRequestBehavior.AllowGet);
+
+        [HttpGet]
+        //httq://server/home/delete?name=xxx, 
+        //顯示詳細資料(等同Details)，提供Submit鈕確認刪除
+        public ActionResult Delete(int? orderId)
+        {
+            return View(orderService.GetOrderById(orderId));
         }
+
+        [HttpPost]//httq://server/home/delete?name=xxx, 送出表單時
+        public ActionResult Delete(int? orderId, FormCollection col)
+        {
+            orderService.GetOrderById(orderId);
+            if (orderService != null)
+                orderService.Delete();
+            return RedirectToAction("Index");
+        }
+
     }
+
+
+    //    [HttpGet()]
+    //    public JsonResult TestJson()
+    //    {
+    //        var result = new Models.Order()
+    //        {
+    //            CustId = "ABC",
+    //            CustName = "測試JSON"
+    //        };
+    //        return this.Json(result, JsonRequestBehavior.AllowGet);
+    //    }
 }
--- mvc/Models/OrderService.cs
+++ mvc/Models/OrderService.cs
@@ -19,11 +19,31 @@ namespace mvc.Models
             return
                 System.Configuration.ConfigurationManager.ConnectionStrings["DBConn"].ConnectionString.ToString();
         }
+        
+        public void Save()
+        {
+            String sql = @"INSERT INTO [Sales].[OrderDetails]([OrderID],[ProductID],[Qty])
+                           VALUES('','@ProductID', '2')";
 
-        /// <summary>
-        /// 新增訂單
-        /// </summary>
-        public void InsertOrder(Models.Order order) {
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.Add(new SqlParameter("@ProductID", ProductID));
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);                
+                conn.Close();
+            }
+        }
+       
+        public void Delete()
+        {
+
+        }
+    
+    /// <summary>
+    /// 新增訂單
+    /// </summary>
+    public void InsertOrder(Models.Order order) {
 
         }
         /// <summary>
@@ -43,7 +63,7 @@ namespace mvc.Models
         /// </summary>
         /// <param name="orderId">訂單ID</param>
         /// <returns></returns>
-        public Models.Order GetOrderById(string orderId) {
+        public Models.Order GetOrderById(int? orderId) {
             Models.Order result = new Order();
             //result.CustId = "kuas";
             //result.CustName = "高應大";
@@ -69,26 +89,54 @@ namespace mvc.Models
             
             foreach (DataRow dr in dt.Rows)
             {
-                // Response.Write(string.Format("OrderId:{0}, OrderDate:{1}, RequiredDate:{2},FirstName:{3},LastName:{4},ProductName:{5},UnitPrice:{6},Qty:{7},Subtotal:{8}<br/>"
-                //     , dr["OrderId"], dr["OrderDate"], dr["RequiredDate"], dr["FirstName"], dr["LastName"], dr["ProductName"], dr["UnitPrice"], dr["Qty"], dr["Subtotal"]));
                 result.OrderId = Convert.ToInt32(dr["OrderId"]);
                 result.CompanyName = Convert.ToString(dr["CompanyName"]);
-                result.OrderDate = Convert.ToDateTime(dr["OrderDate"]);
-                result.ShippedDate = Convert.ToDateTime(dr["ShippedDate"]);              
+                result.OrderDate = Convert.ToString(dr["OrderDate"]);
+                result.ShippedDate = Convert.ToString(dr["ShippedDate"]);              
             }
-
-            return result;           
+            return result;          
 
         }
 
         /// <summary>
-        /// 取得多筆訂單
+        /// 取得訂單列表
         /// </summary>
         /// <returns></returns>
-        public List<Models.Order> GetOrders() {
-            List<Models.Order> result = new List<Order>();
-            result.Add(new Order() { CustId = "kuas", CustName = "高應大", OrderId = 001 });
-            result.Add(new Order() { CustId = "nkfust", CustName = "高第一", OrderId = 002 });
+        public List<Order> GetOrders() {
+            List<Order> result = new List<Order>();
+            //result.Add(new Order() { OrderId = 123, CompanyName = "高應大", OrderDate = Convert.ToDateTime("2017/01/01"),ShippedDate= Convert.ToDateTime("2017/01/01") });
+            DataTable dt = new DataTable();
+            String sql = @"SELECT OrderID,c.CompanyName,LastName+' '+FirstName AS EmpName,ship.CompanyName AS ShipperName,convert(char(10),OrderDate,111) AS OrderDate,convert(char(10),OrderDate,111) AS RequiredDate,convert(char(10),ShippedDate,111) AS ShippedDate
+                           FROM [Sales].[Orders] AS o                           
+	                       JOIN [Sales].[Customers] AS c
+	                       ON o.CustomerID=c.CustomerID
+						   JOIN [HR].[Employees] AS e
+						   ON o.EmployeeID=e.EmployeeID
+						   JOIN [Production].[Suppliers] AS ship
+						   ON o.[ShipperID]=ship.SupplierID";                           
+
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);                
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+
+            result = (from i in dt.AsEnumerable() 
+                      select new Order()
+                      {
+                          OrderId = i.Field<int>("OrderID"),
+                          CompanyName = i.Field<string>("CompanyName"),
+                          EmpName = i.Field<string>("EmpName"),
+                          ShipperName= i.Field<string>("ShipperName"),
+                          OrderDate = i.Field<string>("OrderDate"),
+                          RequireDate = i.Field<string>("RequiredDate"),
+                          ShippedDate = i.Field<string>("ShippedDate")
+
+                      }).ToList<Order>();         
+
             return result;
         }
     }
