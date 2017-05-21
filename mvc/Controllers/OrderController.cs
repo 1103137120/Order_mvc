00ffa1e89f57400ad37599b8597962e3eb9354e8
ASUS <ASUS@ASUS-PCKS>
2017-05-22 04:35:31 +0800

    view更換Kendo UI 之艱辛路程 完成：首頁、新增、查詢 缺：編輯、刪除

--- mvc/Controllers/OrderController.cs
+++ mvc/Controllers/OrderController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using mvc.Models;
-
+using KendoGridBinder;
 
 namespace mvc.Controllers
 {
@@ -18,65 +18,106 @@ namespace mvc.Controllers
         /// <returns></returns>
         public ActionResult Index()
         {
-            return View(orderService.GetOrders());
+            //return View(orderService.GetOrders());
+            return View();
         }
-
         /// <summary>
-        /// 新增訂單頁面
+        /// 取得訂單資料並以Json格式傳到首頁View
         /// </summary>
+        /// <param name="request"></param>
         /// <returns></returns>
-        [HttpGet] 
-        public ActionResult Create()
+        public JsonResult GetOrders(KendoGridRequest request)
         {
-            
-            OrderDetailViewModel viewModel= new OrderDetailViewModel();
-            var od = new List<OrderDetail>();
-            od.Add(new OrderDetail() { });
-            viewModel.OrderDetail = od;
+            var result = orderService.GetOrders();            
+            return Json(new KendoGrid<Order>(request, result), JsonRequestBehavior.AllowGet);
 
+        }
+        public ActionResult Create() {
+            return View();
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]        
+        public JsonResult Create(Order model)
+        {                           
+               orderService.InsertOrder2(model);
+               return Json("新增成功", JsonRequestBehavior.AllowGet);              
+            
+
+        }
+        public JsonResult GetCusDropListItem(KendoGridRequest request)
+        {
             var CusDropListItem = orderService.GetCusDropListItem();
+            return Json(CusDropListItem, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult GetEmpDropListItem(KendoGridRequest request)
+        {
             var EmpDropListItem = orderService.GetEmpDropListItem();
+            return Json(EmpDropListItem, JsonRequestBehavior.AllowGet);
+
+        }
+        public JsonResult GetShipDropListItem(KendoGridRequest request)
+        {
             var ShipDropListItem = orderService.GetShipDropListItem();
-            List<SelectListItem > CustId = new List<SelectListItem>();
-            List<SelectListItem> EmpId = new List<SelectListItem>();
-            List<SelectListItem> ShipperId = new List<SelectListItem>();
-            foreach (var item in CusDropListItem)
-            {
-                CustId.Add(new SelectListItem() { Text=item.CompanyName,Value=item.CustId.ToString()});
-            }
-            foreach (var item in EmpDropListItem)
-            {                
-                EmpId.Add(new SelectListItem() { Text = item.EmpName, Value = item.EmpId.ToString()});
-            }
-            foreach (var item in ShipDropListItem)
-            {
-                ShipperId.Add(new SelectListItem() { Text = item.ShipperName, Value = item.ShipperId.ToString() });
-            }
-            ViewData["CustId"] = CustId;
-            ViewData["EmpId"] = EmpId;
-            ViewData["ShipperId"] = ShipperId;
-            return View(viewModel);
-        }
+            return Json(ShipDropListItem, JsonRequestBehavior.AllowGet);
 
-        /// <summary>
-        /// 新增訂單Action
-        /// </summary>
-        /// <param name="o">整份表單</param>
-        /// <returns></returns>
-        [HttpPost]
-        public ActionResult Create(OrderDetailViewModel o){
-            if (ModelState.IsValid) {
-
-                orderService.InsertOrder(o);
-                int orderId=orderService.getInsertOrderId();
-  //              var orderDetail = new List<OrderDetail>();
-   //            orderDetail = o.OrderDetail.ToList();
-            
-                    orderService.InsertOrderDetail(o,orderId);
-                
-            }          
-            return RedirectToAction("Index");
         }
+        ///// <summary>
+        ///// 新增訂單頁面
+        ///// </summary>
+        ///// <returns></returns>
+        //[HttpGet] 
+        //public ActionResult Create()
+        //{
+
+        //    OrderDetailViewModel viewModel= new OrderDetailViewModel();
+        //    //預設增加1筆訂單明細
+        //    var od = new List<OrderDetail>();
+        //    od.Add(new OrderDetail() { });
+        //    viewModel.OrderDetail = od;
+
+        //    //取出下拉式選單資料並放入SelectListItem
+        //    var CusDropListItem = orderService.GetCusDropListItem();
+        //    var EmpDropListItem = orderService.GetEmpDropListItem();
+        //    var ShipDropListItem = orderService.GetShipDropListItem();
+        //    List<SelectListItem > CustId = new List<SelectListItem>();
+        //    List<SelectListItem> EmpId = new List<SelectListItem>();
+        //    List<SelectListItem> ShipperId = new List<SelectListItem>();
+        //    foreach (var item in CusDropListItem)
+        //    {
+        //        CustId.Add(new SelectListItem() { Text=item.CompanyName,Value=item.CustId.ToString()});
+        //    }
+        //    foreach (var item in EmpDropListItem)
+        //    {                
+        //        EmpId.Add(new SelectListItem() { Text = item.EmpName, Value = item.EmpId.ToString()});
+        //    }
+        //    foreach (var item in ShipDropListItem)
+        //    {
+        //        ShipperId.Add(new SelectListItem() { Text = item.ShipperName, Value = item.ShipperId.ToString() });
+        //    }
+        //    //把剛才設置的SelectListItem放入ViewData並傳至View
+        //    ViewData["CustId"] = CustId;
+        //    ViewData["EmpId"] = EmpId;
+        //    ViewData["ShipperId"] = ShipperId;
+        //    return View(viewModel);
+        //}
+
+        ///// <summary>
+        ///// 新增訂單Action
+        ///// </summary>
+        ///// <param name="o">整份表單</param>
+        ///// <returns></returns>
+        //[HttpPost]
+        //public ActionResult Create(OrderDetailViewModel o){
+        //    if (ModelState.IsValid) {
+        //        //新增訂單
+        //        orderService.InsertOrder(o);
+        //        //取得剛才新增訂單的ID並新增訂單明細
+        //        int orderId=orderService.getInsertOrderId();            
+        //        orderService.InsertOrderDetail(o,orderId);                
+        //    }          
+        //    return RedirectToAction("Index");
+        //}
 
         /// <summary>
         /// 搜尋訂單頁面
@@ -159,6 +200,7 @@ namespace mvc.Controllers
             ViewData["CustIdItem"] = CustId;
             ViewData["EmpIdItem"] = EmpId;
             ViewData["ShipperIdItem"] = ShipperId;
+            //先依訂單ID取得過往訂單，並傳資料到View
             return View(orderService.GetOrderById(orderId));
         }
 
@@ -171,7 +213,9 @@ namespace mvc.Controllers
         [HttpPost]
         public ActionResult Edit(int? orderId, FormCollection form)
         {
+            //取得過往訂單
             var data = orderService.GetOrderById(orderId);
+            //判斷前端修改的值並修改，之後傳入UpdateOrder
             if (TryUpdateModel(data, "", form.AllKeys, new string[] {  }))
             {
                 orderService.UpdateOrder(data);
@@ -208,18 +252,5 @@ namespace mvc.Controllers
                 orderService.DeleteOrder(orderId);
             return RedirectToAction("Index");
         }
-
     }
-
-
-    //    [HttpGet()]
-    //    public JsonResult TestJson()
-    //    {
-    //        var result = new Models.Order()
-    //        {
-    //            CustId = "ABC",
-    //            CustName = "測試JSON"
-    //        };
-    //        return this.Json(result, JsonRequestBehavior.AllowGet);
-    //    }
 }
--- mvc/Models/OrderService.cs
+++ mvc/Models/OrderService.cs
@@ -38,7 +38,7 @@ namespace mvc.Models
             {
                 conn.Open();
                 SqlCommand cmd = new SqlCommand(sql, conn);
-                cmd.Parameters.Add(new SqlParameter("@CustomerID",order.Order.CustId));
+                cmd.Parameters.Add(new SqlParameter("@CustomerID", order.Order.CustId));
                 cmd.Parameters.Add(new SqlParameter("@EmployeeID", order.Order.EmpId));
                 cmd.Parameters.Add(new SqlParameter("@OrderDate", order.Order.OrderDate));
                 cmd.Parameters.Add(new SqlParameter("@RequiredDate", order.Order.RequireDate));
@@ -54,8 +54,42 @@ namespace mvc.Models
                 SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
                 sqlAdapter.Fill(dt);
                 conn.Close();
-            }          
+            }
         }
+        /// <summary>
+        /// 新增訂單2
+        /// </summary>
+        /// <param name="order"></param>
+        public void InsertOrder2(Models.Order order)
+        {
+            Models.Order result = new Order();
+            DataTable dt = new DataTable();
+            String sql = @"INSERT INTO [Sales].[Orders]([CustomerID],[EmployeeID],[OrderDate],[RequiredDate],[ShippedDate],[Freight],[ShipperID],[ShipName],[ShipAddress],[ShipCity],[ShipRegion],[ShipPostalCode],[ShipCountry])
+                           VALUES (@CustomerID,@EmployeeID,@OrderDate,@RequiredDate,@ShippedDate,@Freight,@ShipperID,@ShipName,@ShipAddress,@ShipCity,@ShipRegion,@ShipPostalCode,@ShipCountry)";
+
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.Add(new SqlParameter("@CustomerID", order.CustId));
+                cmd.Parameters.Add(new SqlParameter("@EmployeeID", order.EmpId));
+                cmd.Parameters.Add(new SqlParameter("@OrderDate", order.OrderDate));
+                cmd.Parameters.Add(new SqlParameter("@RequiredDate", order.RequireDate));
+                cmd.Parameters.Add(new SqlParameter("@ShippedDate", order.ShippedDate));
+                cmd.Parameters.Add(new SqlParameter("@Freight", 33));
+                cmd.Parameters.Add(new SqlParameter("@ShipperID", 3));
+                cmd.Parameters.Add(new SqlParameter("@ShipName", "33"));
+                cmd.Parameters.Add(new SqlParameter("@ShipAddress", "twtw"));
+                cmd.Parameters.Add(new SqlParameter("@ShipCity", "taipei"));
+                cmd.Parameters.Add(new SqlParameter("@ShipRegion", "tw"));
+                cmd.Parameters.Add(new SqlParameter("@ShipPostalCode", "321"));
+                cmd.Parameters.Add(new SqlParameter("@ShipCountry", "tw"));
+                SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
+                sqlAdapter.Fill(dt);
+                conn.Close();
+            }
+        }
+
         /// <summary>
         /// 取得最新一筆訂單的ID
         /// </summary>
@@ -63,7 +97,7 @@ namespace mvc.Models
         public int getInsertOrderId()
         {
             Models.Order result = new Order();
-            int orderId=0;
+            int orderId = 0;
             DataTable dt = new DataTable();
             String sql = @"SELECT top 1 OrderID
                            FROM [Sales].[Orders]
@@ -72,7 +106,7 @@ namespace mvc.Models
             using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand(sql, conn);                
+                SqlCommand cmd = new SqlCommand(sql, conn);
                 SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
                 sqlAdapter.Fill(dt);
                 conn.Close();
@@ -89,20 +123,21 @@ namespace mvc.Models
         /// 新增訂單明細
         /// </summary>
         /// <param name="order"></param>
-        public void InsertOrderDetail(Models.OrderDetailViewModel order, int orderId) {
+        public void InsertOrderDetail(Models.OrderDetailViewModel order, int orderId)
+        {
             Models.OrderDetail result = new OrderDetail();
             DataTable dt = new DataTable();
             String sql = @"INSERT INTO [Sales].[OrderDetails]([OrderID],[ProductID],[UnitPrice],[Qty])
                            VALUES (@OrderID,@ProductID,@UnitPrice,@Qty)";
-            
+
             using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand(sql, conn);                
+                SqlCommand cmd = new SqlCommand(sql, conn);
                 cmd.Parameters.Add(new SqlParameter("@OrderID", orderId));
                 cmd.Parameters.Add(new SqlParameter("@ProductID", order.OrderDetail[0].ProductId));
                 cmd.Parameters.Add(new SqlParameter("@UnitPrice", order.OrderDetail[0].UnitPrice));
-                cmd.Parameters.Add(new SqlParameter("@Qty", order.OrderDetail[0].Qty));                
+                cmd.Parameters.Add(new SqlParameter("@Qty", order.OrderDetail[0].Qty));
 
                 SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
                 sqlAdapter.Fill(dt);
@@ -110,17 +145,18 @@ namespace mvc.Models
             }
         }
 
-        public List<Order> SearchOrder(Models.Order order) {
+        public List<Order> SearchOrder(Models.Order order)
+        {
             List<Order> result = new List<Order>();
             DataTable dt = new DataTable();
             String sqlwhere = "1=1";
             if (order.OrderId != 0)
             {
-                sqlwhere = sqlwhere+ "AND OrderID=@OrderID";
+                sqlwhere = sqlwhere + "AND OrderID=@OrderID";
             }
             if (order.CustId != 0)
             {
-                sqlwhere = sqlwhere + " AND c.CustomerID=@CustomerID";                
+                sqlwhere = sqlwhere + " AND c.CustomerID=@CustomerID";
             }
             if (order.EmpId != 0)
             {
@@ -150,20 +186,20 @@ namespace mvc.Models
 						   ON o.EmployeeID=e.EmployeeID
 						   JOIN [Production].[Suppliers] AS ship
 						   ON o.[ShipperID]=ship.SupplierID
-                           WHERE "+ sqlwhere;
+                           WHERE " + sqlwhere;
 
-                using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
+            using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
             {
                 conn.Open();
                 SqlCommand cmd = new SqlCommand(sql, conn);
-                if (order.OrderId !=0) { cmd.Parameters.Add(new SqlParameter("@OrderID", order.OrderId));  }
+                if (order.OrderId != 0) { cmd.Parameters.Add(new SqlParameter("@OrderID", order.OrderId)); }
                 if (order.CustId != 0) { cmd.Parameters.Add(new SqlParameter("@CustomerID", order.CustId)); }
-                if (order.EmpId!= 0) { cmd.Parameters.Add(new SqlParameter("@EmployeeID", order.EmpId));}
+                if (order.EmpId != 0) { cmd.Parameters.Add(new SqlParameter("@EmployeeID", order.EmpId)); }
                 if (order.ShipperName != null) { cmd.Parameters.Add(new SqlParameter("@ShipperName", order.ShipperName)); }
                 if (order.OrderDate != null) { cmd.Parameters.Add(new SqlParameter("@OrderDate", order.OrderDate)); }
-                if (order.RequireDate != null) { cmd.Parameters.Add(new SqlParameter("@RequiredDate", order.RequireDate));}
+                if (order.RequireDate != null) { cmd.Parameters.Add(new SqlParameter("@RequiredDate", order.RequireDate)); }
                 if (order.ShippedDate != null) { cmd.Parameters.Add(new SqlParameter("@ShippedDate", order.ShippedDate)); }
-                
+
                 SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
                 sqlAdapter.Fill(dt);
                 conn.Close();
@@ -190,9 +226,12 @@ namespace mvc.Models
         /// <summary>
         /// 刪除訂單
         /// </summary>
-        public void DeleteOrder(int? orderId) {
+        public void DeleteOrder(int? orderId)
+        {
             DataTable dt = new DataTable();
-            String sql = @"DELETE FROM [Sales].[Orders]
+            String sql = @"DELETE FROM [Sales].[OrderDetails]
+                           WHERE [OrderID]=@OrderId
+                           DELETE FROM [Sales].[Orders]
                            WHERE [OrderID]=@OrderId";
 
             using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
@@ -209,7 +248,8 @@ namespace mvc.Models
         /// 修改訂單
         /// </summary>
         /// <param name="order"></param>
-        public void UpdateOrder(Models.Order order) {
+        public void UpdateOrder(Models.Order order)
+        {
             DataTable dt = new DataTable();
             String sql = @"UPDATE [Sales].[Orders]
                            SET [CustomerID]=@CustomerID,[EmployeeID]=@EmployeeID,[OrderDate]=@OrderDate,[RequiredDate]=@RequiredDate,[ShippedDate]=@ShippedDate,[Freight]=@Freight,[ShipperID]=@ShipperID,[ShipName]=@ShipName,[ShipAddress]=@ShipAddress,[ShipCity]=@ShipCity,[ShipRegion]=@ShipRegion,[ShipPostalCode]=@ShipPostalCode,[ShipCountry]=@ShipCountry
@@ -244,7 +284,8 @@ namespace mvc.Models
         /// </summary>
         /// <param name="orderId">訂單ID</param>
         /// <returns></returns>
-        public Models.Order GetOrderById(int? orderId) {
+        public Models.Order GetOrderById(int? orderId)
+        {
             Models.Order result = new Order();
 
             DataTable dt = new DataTable();
@@ -264,39 +305,40 @@ namespace mvc.Models
                 SqlCommand cmd = new SqlCommand(sql, conn);
                 cmd.Parameters.Add(new SqlParameter("@OrderId", orderId));
                 SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
-                sqlAdapter.Fill(dt);                
-                conn.Close();                    
+                sqlAdapter.Fill(dt);
+                conn.Close();
             }
-            
+
             foreach (DataRow dr in dt.Rows)
             {
                 result.OrderId = Convert.ToInt32(dr["OrderID"]);
                 result.CustId = Convert.ToInt32(dr["CustomerID"]);
                 result.EmpId = Convert.ToInt32(dr["EmployeeID"]);
                 result.CompanyName = Convert.ToString(dr["CompanyName"]);
-                result.EmpName= Convert.ToString(dr["EmpName"]);
-                result.ShipperId = Convert.ToInt32(dr["ShipperID"]); 
+                result.EmpName = Convert.ToString(dr["EmpName"]);
+                result.ShipperId = Convert.ToInt32(dr["ShipperID"]);
                 result.ShipperName = Convert.ToString(dr["ShipperName"]);
                 result.OrderDate = Convert.ToString(dr["OrderDate"]);
                 result.RequireDate = Convert.ToString(dr["RequiredDate"]);
                 result.ShippedDate = Convert.ToString(dr["ShippedDate"]);
-                result.Freight = Convert.ToInt32(dr["Freight"]); 
-                result.ShipName=Convert.ToString(dr["ShipName"]);             
-                result.ShipAddress= Convert.ToString(dr["ShipAddress"]);
-                result.ShipCity= Convert.ToString(dr["ShipCity"]);
-                result.ShipRegion= Convert.ToString(dr["ShipRegion"]);
-                result.ShipPostalCode= Convert.ToString(dr["ShipPostalCode"]);
-                result.ShipCountry= Convert.ToString(dr["ShipCountry"]);           
-                   
+                result.Freight = Convert.ToInt32(dr["Freight"]);
+                result.ShipName = Convert.ToString(dr["ShipName"]);
+                result.ShipAddress = Convert.ToString(dr["ShipAddress"]);
+                result.ShipCity = Convert.ToString(dr["ShipCity"]);
+                result.ShipRegion = Convert.ToString(dr["ShipRegion"]);
+                result.ShipPostalCode = Convert.ToString(dr["ShipPostalCode"]);
+                result.ShipCountry = Convert.ToString(dr["ShipCountry"]);
+
             }
-            return result;          
+            return result;
         }
 
         /// <summary>
         /// 取得訂單列表
         /// </summary>
         /// <returns></returns>
-        public List<Order> GetOrders() {
+        public List<Order> GetOrders()
+        {
             List<Order> result = new List<Order>();
             DataTable dt = new DataTable();
             String sql = @"SELECT OrderID,c.CompanyName,LastName+' '+FirstName AS EmpName,ship.CompanyName AS ShipperName,convert(char(10),OrderDate,111) AS OrderDate,convert(char(10),OrderDate,111) AS RequiredDate,convert(char(10),ShippedDate,111) AS ShippedDate
@@ -306,29 +348,29 @@ namespace mvc.Models
 						   JOIN [HR].[Employees] AS e
 						   ON o.EmployeeID=e.EmployeeID
 						   JOIN [Production].[Suppliers] AS ship
-						   ON o.[ShipperID]=ship.SupplierID";                         
+						   ON o.[ShipperID]=ship.SupplierID";
 
             using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
             {
                 conn.Open();
-                SqlCommand cmd = new SqlCommand(sql, conn);                
+                SqlCommand cmd = new SqlCommand(sql, conn);
                 SqlDataAdapter sqlAdapter = new SqlDataAdapter(cmd);
                 sqlAdapter.Fill(dt);
                 conn.Close();
             }
 
-            result = (from i in dt.AsEnumerable() 
+            result = (from i in dt.AsEnumerable()
                       select new Order()
                       {
                           OrderId = i.Field<int>("OrderID"),
                           CompanyName = i.Field<string>("CompanyName"),
                           EmpName = i.Field<string>("EmpName"),
-                          ShipperName= i.Field<string>("ShipperName"),
+                          ShipperName = i.Field<string>("ShipperName"),
                           OrderDate = i.Field<string>("OrderDate"),
                           RequireDate = i.Field<string>("RequiredDate"),
                           ShippedDate = i.Field<string>("ShippedDate")
 
-                      }).ToList<Order>();         
+                      }).ToList<Order>();
 
             return result;
         }
@@ -341,13 +383,7 @@ namespace mvc.Models
             List<Models.Order> MapResult = new List<Order>();
             DataTable dt = new DataTable();
             String sql = @"SELECT CustomerID,CompanyName
-                           FROM [Sales].[Customers]
-
-                           SELECT EmployeeID,LastName+' '+FirstName AS EmpName
-                           FROM [HR].[Employees]
-
-                           SELECT ShipperID,CompanyName AS ShipperName
-                           FROM [Sales].[Shippers]";
+                           FROM [Sales].[Customers] ";
 
             using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
             {
@@ -379,10 +415,7 @@ namespace mvc.Models
             List<Models.Order> MapResult = new List<Order>();
             DataTable dt = new DataTable();
             String sql = @"SELECT EmployeeID,LastName+' '+FirstName AS EmpName
-                           FROM [HR].[Employees]
-
-                           SELECT ShipperID,CompanyName AS ShipperName
-                           FROM [Sales].[Shippers]";
+                           FROM [HR].[Employees] ";
 
             using (SqlConnection conn = new SqlConnection(this.GetDBConnectionString()))
             {
@@ -442,7 +475,7 @@ namespace mvc.Models
         /// </summary>
         /// <param name="OrderData"></param>
         /// <returns></returns>
-        public List<Models.Order> MapOrderDropListData(DataTable OrderData)
+        public List<Models.Order> MapOrderData(DataTable OrderData)
         {
             List<Models.Order> MapResult = new List<Order>();
             foreach (DataRow Row in OrderData.Rows)
